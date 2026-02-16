a9735d0e73b9418a59af25076223addd8227a5a9
st403 <st403@WS403-06>
2026-02-16 09:32:52 +0300

    Подключиласть к базе данных

---
+++ Kinoteatr_Web_2027/Date/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Kinoteatr_Web_2027.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kinoteatr_Web_2027.Date
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        {
+            //Database.Migrate();
+        }
+
+        public DbSet<Ticket> Books { get; set; }
+        public DbSet<Version> Students { get; set; }
+
+    }
+}
