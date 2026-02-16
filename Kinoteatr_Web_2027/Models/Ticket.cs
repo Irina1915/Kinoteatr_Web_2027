dc587a681ac22c2b4fa857f4757d06ae12b42bac
st403 <st403@WS403-06>
2026-02-16 08:57:40 +0300

    Создала EFModel

---
+++ Kinoteatr_Web_2027/Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿namespace Kinoteatr_Web_2027.Models
+{
+    public class Ticket : EFModel
+    {
+        public string Title { get; set; }
+        public string Viewer { get; set; }
+        public int Date { get; set; }
+
+        public double Summa { get; set; }
+    }
+}
