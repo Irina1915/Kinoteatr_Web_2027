580c3b244dcd0bb5603da86a3c30f6597d97b7c5
st52 <st52@WS52-08>
2026-04-25 14:11:39 +0300

    Выполнила еще один тест с Hub

---
+++ Test_Kinoteatr_Web/Hubs/TicketHubTests.cs
@@ -0,0 +1,49 @@
+﻿using Kinoteatr_Web_2027.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kinoteatr_Web_2027.Hubs;
+using Moq;
+
+namespace Test_Kinoteatr_Web.Hubs
+{
+    public class TicketHubTests
+    {
+        [Fact]
+        public async Task SendTicketUpdate_ShouldSendMessageToAllClients()
+        {
+            // Arrange
+            var hub = new TicketHub();
+
+            var clientsMock = new Mock<IHubCallerClients>();
+            var clientProxyMock = new Mock<IClientProxy>();
+
+            clientsMock.Setup(c => c.All).Returns(clientProxyMock.Object);
+
+            hub.Clients = clientsMock.Object;
+
+            var ticket = new Ticket
+            {
+                Title = "Test Book",
+                Date = DateTime.Now.AddDays(30),
+                Summa = 1500
+            };
+
+            // Act
+            await hub.SendTicketUpdate(ticket);
+
+            // Assert
+            clientProxyMock.Verify(
+                c => c.SendCoreAsync(
+                    "TicketUpdated",
+                    It.Is<object[]>(o => o.Length == 1 && o[0] == ticket),
+                    default
+                ),
+                  Times.Once
+            );
+        }
+    }
+}
